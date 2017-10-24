48c3506d8afd50725808e6524136fd81e735b2af
Jeffrey Allen <allenjeffreyl@gmail.com>
2017-10-23 21:50:10 -0400

    still trying to fix Heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,17 @@ var mysql = require("mysql");
 
 var connection;
 
-  if (process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-  }
-  else{
-    connection = mysql.createConnection({
+}
+else {
+  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "Determined08!",
     database: "burgers_db"
   });
 };
-
 
 connection.connect(function(err) {
   if (err) {
